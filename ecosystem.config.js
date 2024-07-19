aae93eadeefc0feef57746865f63e5b53aa10072
Murtaza Singapurwala <66314258+Murtaza6714@users.noreply.github.com>
2024-07-19 21:25:37 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "next-app", // Choose a unique name for your app
         script: "node_modules/next/dist/bin/next", // Path to Next.js executable
-        args: "start", // Command to start Next.js server
+        args: "start -port 4000", // Command to start Next.js server
         watch: true // Set to true if you want PM2 to watch for file changes and restart the app
       }
     ]
